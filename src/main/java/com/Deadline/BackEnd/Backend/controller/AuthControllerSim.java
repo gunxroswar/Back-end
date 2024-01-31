80510bda3a69c23ada2c5379d04412ce95bd63d4
BT_kung <u.nantanat@gmail.com>
2024-02-01 04:33:10 +0700

    Add Auth_Simple

---
+++ src/main/java/com/Deadline/BackEnd/Backend/controller/AuthControllerSim.java
@@ -0,0 +1,41 @@
+package com.Deadline.BackEnd.Backend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.Deadline.BackEnd.Backend.userLogin.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@RestController
+public class AuthControllerSim {
+    private List<User> user = new ArrayList<>();
+
+    private final AtomicLong counter = new AtomicLong();
+    public AuthControllerSim(){
+
+    }
+
+    @PostMapping("/guests/register")
+    public void register(@RequestBody User u){
+        user.add(new User(counter.getAndIncrement(),u.getDisplay_name(),u.getUsername(),u.getPassword()));
+    }
+
+    @PostMapping("/guest/login")
+    public boolean login(@RequestBody User u){
+        long id = -1;
+        for (int i = 0 ;i<user.size();i++){
+            if (u.getUsername().equals(user.get(i).getUsername())){
+                id = i;
+            }
+        }
+        if(u.getPassword().equals(user.get((int) id).getPassword())){
+            return true;
+        }
+        return false;
+    }
+
+}
