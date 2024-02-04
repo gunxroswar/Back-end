ebec3460043247284bc04f47e1d64a2f2f3118f1
Natee-wtwsk <gg977120@gmail.com>
2024-02-05 00:12:21 +0700

    Create Create and Get comment.

--- src/main/java/com/Deadline/BackEnd/Backend/Objects/Payload.java
+++ src/main/java/com/Deadline/BackEnd/Backend/Objects/Payload.java
@@ -1,6 +1,9 @@
 package com.Deadline.BackEnd.Backend.Objects;
 
+import java.math.BigInteger;
+
 public class Payload {
+    public BigInteger PostID;
     public String topic;
     public String detail;
     public String cookie;
