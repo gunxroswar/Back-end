d7b04dc968162d63b1c456eb96cfcff6fb7403e8
BT_kung <u.nantanat@gmail.com>
2024-03-06 01:14:47 +0700

    Track ReplyNotFoundException

---
+++ src/main/java/com/Deadline/BackEnd/Backend/exception/ReplyNotFoundException.java
@@ -0,0 +1,14 @@
+package com.Deadline.BackEnd.Backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ReplyNotFoundException extends RuntimeException {
+
+    public ReplyNotFoundException(Long replyId)
+    {
+        super("Reply found with "+replyId);
+    }
+
+}
