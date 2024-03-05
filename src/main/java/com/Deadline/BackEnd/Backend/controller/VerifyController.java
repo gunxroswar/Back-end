180accd32aea12512c6443d1ed5f7a7a50337f38
BT_kung <u.nantanat@gmail.com>
2024-03-06 02:32:23 +0700

    Fix bookmark, like, post, and verify

--- src/main/java/com/Deadline/BackEnd/Backend/controller/VerifyController.java
+++ src/main/java/com/Deadline/BackEnd/Backend/controller/VerifyController.java
@@ -14,10 +14,7 @@ import com.fasterxml.jackson.databind.node.POJONode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -33,7 +30,7 @@ public class VerifyController {
 
 
     @PutMapping ("/comments/verify")
-    public ResponseEntity<String> verifyComment(@RequestHeader("Authorization") String authorizationHeader,@PathVariable("comment_id") long commentId) {
+    public ResponseEntity<String> verifyComment(@RequestHeader("Authorization") String authorizationHeader,@RequestParam("comment_id") long commentId) {
         String bearerToken = authorizationHeader.replace("Bearer ", "");
         Long uid = Long.parseLong(jwt.extractUID(bearerToken));
         try{
@@ -60,7 +57,7 @@ public class VerifyController {
     }
 
     @PutMapping ("/comments/unverify")
-    public ResponseEntity<String> unverifyComment(@RequestHeader("Authorization") String authorizationHeader,@PathVariable("comment_id") long commentId) {
+    public ResponseEntity<String> unverifyComment(@RequestHeader("Authorization") String authorizationHeader,@RequestParam("comment_id") long commentId) {
         String bearerToken = authorizationHeader.replace("Bearer ", "");
         Long uid = Long.parseLong(jwt.extractUID(bearerToken));
         try{
